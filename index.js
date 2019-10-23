71a907cf0dbeb8f49fb27be4ea30b2568eb03339
Dhruv Soni <dhruv.soni@hashedin.com>
2019-10-23 09:06:54 +0530

    added a podspec file

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
+import {NativeModules} from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const {Wifi} = NativeModules;
 
-AppRegistry.registerComponent(appName, () => App);
+export default Wifi;
